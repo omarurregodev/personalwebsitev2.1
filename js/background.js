1c2e88065f057b7a53ecd2ee01901b0f4f3196cb
omarurregodev <omarurrego07@gmail.com>
2022-05-20 17:52:08 -0500

    agregue las secciones about me, my skills y portfolio

--- js/background.js
+++ js/background.js
@@ -73,8 +73,8 @@ function setup() {
   canvas = createCanvas(body_width, body_height);
   canvas.position(0,0);
   canvas.style('z-index','-1');
-  if(particles.length <= 80) {
-    for(let i = 0;i<80;i++){
+  if(particles.length <= windowWidth/4) {
+    for(let i = 0;i<windowWidth/4;i++){
       particles.push(new Particle());
     }
   }
