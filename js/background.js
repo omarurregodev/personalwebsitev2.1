911d8e563ca97aa9947f0cbde8ac108922951b6d
omarurregodev <omarurrego07@gmail.com>
2022-05-24 23:10:35 -0500

    background particle modified

--- js/background.js
+++ js/background.js
@@ -73,8 +73,8 @@ function setup() {
   canvas = createCanvas(body_width,body_height);
   canvas.position(0,0);
   canvas.style('z-index','-1');
-  if(particles.length <= windowWidth/10) {
-    for(let i = 0;i<windowWidth/10;i++){
+  if(particles.length <= windowWidth/15) {
+    for(let i = 0;i<windowWidth/15;i++){
       particles.push(new Particle());
     }
   }
